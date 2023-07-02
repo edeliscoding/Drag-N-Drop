459475bee768d638a3d3920b1d44505bbc438507
edeliscoding <edelreichd@gmail.com>
2023-07-02 12:14:22 -0600

    fixed myfiles page

--- app/myfiles/page.jsx
+++ app/myfiles/page.jsx
@@ -1,20 +1,13 @@
 "use client";
-import Head from "next/head";
-import Script from "next/script";
+
 import { useEffect, useRef } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 export default function MyPage() {
   const containerRef = useRef(null);
+
   const session = useSession();
   const router = useRouter();
-
-  if (session.status === "loading") {
-    return <p>Loading...</p>;
-  }
-  if (session.status === "unauthenticated") {
-    router?.push("/login");
-  }
 
   useEffect(() => {
     cloudinary
@@ -31,6 +24,13 @@ export default function MyPage() {
       })
       .render();
   }, []);
+  if (session.status === "loading") {
+    return <p>Loading...</p>;
+  }
+  if (session.status === "unauthenticated") {
+    router?.push("/login");
+  }
+
   return (
     <div ref={containerRef} className="max-w-7xl mx-auto mt-10" id="my-gallery">
       Gallery Page
